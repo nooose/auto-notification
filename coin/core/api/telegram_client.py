f31958653c2be3a754f6dd6a2a0d7d45972145be
nooose <hd15807@gmail.com>
2025-02-24 11:29:56 +0900

    feat: 비동기 텔레그램 호출 함수 추가

--- coin/core/api/telegram_client.py
+++ coin/core/api/telegram_client.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import requests
 
 from core.api.telegram_properties import TelegramProperties
@@ -15,22 +16,51 @@ class TelegramClient:
 
         self.bot_token: str = properties.bot_token
         self.chat_id: str = properties.chat_id
+        self.url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
+
+
+    def async_send_message(self, message):
+        """동기 코드에서 비동기 메시지 전송을 호출하는 래퍼
+        """
+
+        asyncio.get_event_loop().create_task(self._async_send_message(message))
+
+    async def _async_send_message(self, message):
+        """텔레그램에 비동기로 메시지를 전송한다.
+
+        :param message: 전송할 메시지
+        :return:
+        """
+
+        payload = self._create_payload(message)
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(self.url, data=payload) as response:
+                    if response.status != 200:
+                        raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status)
+                    print(f"비동기 메시지 전송 성공: {message}")
+        except aiohttp.ClientError as e:
+            print(f"비동기 텔레그램 메시지 전송 실패: {e}")
 
     def send_message(self, message):
-        """
-        메시지를 비동기로 전송한다.
+        """텔레그램에 메시지를 전송한다.
 
-        :param message: 채널에 전달할 메시지
+        :param message: 전송할 메시지
+        :return:
         """
-        url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
-        payload = {
-            "chat_id": self.chat_id,
-            "text": message,
-            "parse_mode": "Markdown"
-        }
-
+                
+        payload = self._create_payload(message)
         try:
-            response = requests.post(url, data=payload)
+            response = requests.post(self.url, data=payload)
             response.raise_for_status()
         except requests.RequestException as e:
             print(f"텔레그램 메시지 전송 실패: {e}")
+
+    def _create_payload(self, message: str) -> Dict:
+        """텔레그램 API 요청에 사용되는 payload를 생성한다.
+
+        :param message: 전송할 메시지
+        :return: payload
+        """
+
+        return {"chat_id": self.chat_id, "text": message, "parse_mode": "Markdown"}
